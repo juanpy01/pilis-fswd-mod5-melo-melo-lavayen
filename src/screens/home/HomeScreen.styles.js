06d1ef3731125a2b4be330275dc0ad2a3cf914cb
Juanpy01 <melo.juanp@gmail.com>
2023-04-15 16:49:26 -0300

    first commit

---
+++ src/screens/home/HomeScreen.styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet, StatusBar } from 'react-native'
+import { FONT_SIZE } from '../../utils/theme'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight
+  },
+  bgImage: {
+    resizeMode: 'cover',
+    width: '100%',
+    height: 900,
+    justifyContent: 'center',
+    top: -50
+  },
+  title: {
+    fontSize: FONT_SIZE.xxxl,
+    fontWeight: 'bold',
+    color: '#03001C',
+    width: '75%',
+    marginLeft: 25,
+    top: -150
+  }
+})
---
+++ src/utils/theme.js
@@ -0,0 +1,28 @@
+export const COLORS = {
+  primary: '#FF6000',
+  secondary: '#FFA559',
+  text: '#454545',
+  inactive: '#A0A0A0',
+  white: '#FFE6C7',
+  grey: '#F1F1F1'
+}
+
+export const SPACING = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 40,
+  xxl: 48,
+  xxxl: 64
+}
+
+export const FONT_SIZE = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 40,
+  xxl: 48,
+  xxxl: 64
+}
