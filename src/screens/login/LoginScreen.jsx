2892e6bebb1fb53061748d1b268ce1050be71560
Luis Lavayen <lu14393@gmail.com>
2023-04-18 18:19:36 -0300

    correccion menor

--- src/screens/login/LoginScreen.jsx
+++ src/screens/login/LoginScreen.jsx
@@ -71,24 +71,6 @@ export const LoginScreen = () => {
       <TouchableOpacity style={styles.button} onPress={handleSubmit(handleLogin)}>
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
-
-      <Button title={'Sign in with Google'} onPress={() => {
-        GoogleSignin.configure({
-          androidClientId: 'ADD_YOUR_ANDROID_CLIENT_ID_HERE',
-          iosClientId: 'ADD_YOUR_iOS_CLIENT_ID_HERE',
-        });
-        GoogleSignin.hasPlayServices().then((hasPlayService) => {
-          if (hasPlayService) {
-            GoogleSignin.signIn().then((userInfo) => {
-              console.log(JSON.stringify(userInfo))
-            }).catch((e) => {
-              console.log("ERROR IS: " + JSON.stringify(e));
-            })
-          }
-        }).catch((e) => {
-          console.log("ERROR IS: " + JSON.stringify(e));
-        })
-      }} />
     </View>
   )
 };
