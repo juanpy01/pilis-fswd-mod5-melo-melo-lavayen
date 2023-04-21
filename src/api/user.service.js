3620384d44a42354e6d79da601a84112b074ee64
Juanpy01 <melo.juanp@gmail.com>
2023-04-20 21:33:39 -0300

    se actualizo user.service

---
+++ src/api/user.service.js
@@ -0,0 +1,12 @@
+import { api } from './api'
+
+const ENDPOINT = `${api.server + api.apiVersion}/users`
+
+export const getEventList = async () => {
+  try {
+    const response = await fetch(ENDPOINT)
+    return response.json()
+  } catch {
+    throw new Error('could not fetch users')
+  }
+}
